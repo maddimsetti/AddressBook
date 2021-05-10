0ecd404b815be10defbee5d149dfd7e0dd14f6cd
prudhvi <prudhvieethakota123@gmail.com>
2021-05-10 11:42:18 +0530

    Added:Code for Creating Contacts in Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,35 @@
 package com.addressbook;
 
 public class AddressBook {
+    String firstName,lastName,address,city,state,eMail,phoneNumber;
+    int zip;
+
+    public AddressBook (String firstName,String lastName,String address,String city,String state,
+                 int zip,String eMail, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addressBook() {
+        System.out.println("First Name: "+firstName);
+        System.out.println("Last Name: "+lastName);
+        System.out.println("Address: "+address);
+        System.out.println("City: "+city);
+        System.out.println("State: "+state);
+        System.out.println("Zip: "+zip);
+        System.out.println("Email Address: "+eMail);
+        System.out.println("Phone Number: "+phoneNumber);
+    }
     public static void main (String[] args) {
         System.out.println("Welcome to Address Book Program in AddressBook in Main Class");
+        AddressBook book = new AddressBook(null,null,null,null,null,0,null,null);
+        book.addressBook();
     }
 
 }
